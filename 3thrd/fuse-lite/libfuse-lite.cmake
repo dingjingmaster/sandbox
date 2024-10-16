774242bbc336dca553a5d726c350c27cbec22a21
dingjing <dingjing@live.cn>
2024-10-16 18:04:14 +0800

    1. fuse-lite 2. filesystem

---
+++ 3thrd/fuse-lite/libfuse-lite.cmake
@@ -0,0 +1,15 @@
+file(GLOB FUSE_LITE_SRC
+        fuse.c
+        fuse_i.h
+        fuse_kern_chan.c
+        fuse_loop.c
+        fuse_lowlevel.c
+        fuse_misc.h
+        fuse_opt.c
+        fuse_session.c
+        fuse_signals.c
+        fusermount.c
+        helper.c
+        mount.c
+        mount_util.c mount_util.h
+)
